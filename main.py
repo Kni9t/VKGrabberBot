5cbc4221d3628dff16f1c3e6fd8290e494517009
Kni9t <clone111@bk.ru>
2025-05-05 19:30:55 +0300

    add send test massage

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from VKGrabber import VKGrabber
 
 VKCollector = VKGrabber("Parameters/token")
 bot = telebot.TeleBot(open('Parameters/botKey').read())
+channelParams = json.load('channel_parameters.json')
+
+bot.send_message(chat_id = channelParams['channel_username'], text = 'Hello world!')
