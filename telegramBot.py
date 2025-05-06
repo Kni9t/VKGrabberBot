19960a644a29de9c58b2f11a5d551389595ba0df
Kni9t <clone111@bk.ru>
2025-05-06 17:09:07 +0300

    Add history for exist posts

--- telegramBot.py
+++ telegramBot.py
@@ -2,52 +2,78 @@ import telebot
 from telebot import types
 import requests
 import time
+import hashlib
+import json
 
 class ObserverBot:
     def __init__(self, botkey, channelName):
         self.bot = telebot.TeleBot(botkey)
         self.channelName = channelName
         
+    def generateContentHash(self, caption, url = ''):
+        data = (caption or '') + str(url)
+        return hashlib.md5(data.encode()).hexdigest()
+        
     def SendPost(self, posts):
         if (type(posts) is list):
             posts = reversed(posts)
+            
+        try:
+            with open('sent_posts.json', 'r') as f:
+                sent_posts = list(json.load(f))
+                f.close()
+        except:
+            sent_posts = []
         
-        for post in posts:
-            media_group = []
-            music_group = []
-            doc_group = []
+        try:
+            for post in posts:
+                media_group = []
+                music_group = []
+                doc_group = []
                 
-            for mediaLink in post['mediaLinks']:
-                if ((mediaLink['type'] == 'photo') or (mediaLink['type'] == 'video')):
-                    media_group.append(types.InputMediaPhoto(mediaLink['content']))
-                
-                if (mediaLink['type'] == 'gif'):
-                    media_group.append(types.InputMediaDocument(media = mediaLink['content']))
-                
-                if (mediaLink['type'] == 'doc'):
-                    doc_group.append(mediaLink)
+                if (self.generateContentHash(post['text'], post['mediaLinks']) in sent_posts):
+                    print(f'Пост ({post['text'][:40]}) уже опубликован!')
+                    continue
                     
-                if (mediaLink['type'] == 'audio'):
-                    music_group.append(mediaLink)
+                for mediaLink in post['mediaLinks']:
+                    if ((mediaLink['type'] == 'photo') or (mediaLink['type'] == 'video')):
+                        media_group.append(types.InputMediaPhoto(mediaLink['content']))
                     
-            if (len(media_group) > 0):
-                if (len(post['text']) <= 1024):
-                    media_group[0].caption = post['text']
-                    self.bot.send_media_group(chat_id = self.channelName, media = media_group)
+                    if (mediaLink['type'] == 'gif'):
+                        media_group.append(types.InputMediaDocument(media = mediaLink['content']))
+                    
+                    if (mediaLink['type'] == 'doc'):
+                        doc_group.append(mediaLink)
+                        
+                    if (mediaLink['type'] == 'audio'):
+                        music_group.append(mediaLink)
+                        
+                if (len(media_group) > 0):
+                    if (len(post['text']) <= 1024):
+                        media_group[0].caption = post['text']
+                        self.bot.send_media_group(chat_id = self.channelName, media = media_group)
+                    else:
+                        self.bot.send_media_group(chat_id = self.channelName, media = media_group)
+                        self.bot.send_message(chat_id = self.channelName, text = post['text'])
                 else:
-                    self.bot.send_media_group(chat_id = self.channelName, media = media_group)
-                    self.bot.send_message(chat_id = self.channelName, text = post['text'])
-            else:
-                if (post['text'] != ''): self.bot.send_message(chat_id = self.channelName, text = post['text'])
-                
-            if (len(music_group) > 0):
-                for music in music_group:
-                    self.bot.send_audio(chat_id = self.channelName,
-                                        audio = requests.get(music['content']).content,
-                                        title = music['title'],
-                                        performer = music['artist'])
+                    if (post['text'] != ''): self.bot.send_message(chat_id = self.channelName, text = post['text'])
                     
-            if (len(doc_group) > 0):
-                for doc in doc_group:
-                    self.bot.send_document(chat_id = self.channelName, document = doc['content'])
-        time.sleep(5)
+                if (len(music_group) > 0):
+                    for music in music_group:
+                        self.bot.send_audio(chat_id = self.channelName,
+                                            audio = requests.get(music['content']).content,
+                                            title = music['title'],
+                                            performer = music['artist'])
+                        
+                if (len(doc_group) > 0):
+                    for doc in doc_group:
+                        self.bot.send_document(chat_id = self.channelName, document = doc['content'])
+                
+                sent_posts.append(self.generateContentHash(post['text'], post['mediaLinks']))
+                time.sleep(3)
+                
+            with open('sent_posts.json', 'w', encoding='utf-8') as f:
+                json.dump(sent_posts, f)
+        except:
+            with open('sent_posts.json', 'w', encoding='utf-8') as f:
+                json.dump(sent_posts, f)
