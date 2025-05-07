907e09c2b3f30b4ca12029a037b632c0bfc12372
Kni9t <clone111@bk.ru>
2025-05-07 16:10:04 +0300

    add time controller for bots

---
+++ timeController.py
@@ -0,0 +1,32 @@
+import logging
+from datetime import datetime, timedelta
+
+class TimeController:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        
+    def sleepToNextHalfHour(self):
+        
+        try:
+            now = datetime.now()
+    
+            if (now.minute < 30):
+                minute = 0 
+            else:
+                minute = 30
+            
+            next_run = now.replace(minute = minute, second=0, microsecond=0)
+            
+            if now >= next_run:
+                next_run += timedelta(minutes = 30)
+                
+            sleep_seconds = int((next_run - now).total_seconds())
+            
+            self.logger.info(f'Ожидаю {sleep_seconds} сек. до: {next_run}')
+            
+            #time.sleep(sleep_seconds)
+        except Exception as e:
+            msg = f'Ошибка при попытке выполнить ожидание до {next_run}: {e}'
+            
+            print(msg)
+            self.logger.error(msg)
